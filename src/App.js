f5e511c70802eb96e00ccdd58ad4190c051f71ff
Justin Huang <justin.2260@gmail.com>
2022-02-28 20:30:23 -0500

    update card images

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 const cardImages = [
-  { "src": "https://img.icons8.com/cotton/128/000000/cat--v4.jpg", matched: false },
-  { "src": "https://img.icons8.com/cotton/128/000000/dog--v2.jpg", matched: false },
-  { "src": "https://img.icons8.com/cotton/128/000000/pigeon.jpg", matched: false },
-  { "src": "https://img.icons8.com/cotton/128/000000/pigeon--v1.jpg", matched: false },
-  { "src": "https://img.icons8.com/cotton/128/000000/pigeon--v2.jpg", matched: false },
-  { "src": "https://img.icons8.com/cotton/128/000000/pigeon--v3.jpg", matched: false },
-  { "src": "https://img.icons8.com/cotton/128/000000/owl--v2.jpg", matched: false },
-  { "src": "https://img.icons8.com/cotton/128/000000/cow-breed.jpg", matched: false }
+  { "icon": "fa-solid fa-bird", matched: false },
+  { "icon": "fa-regular fa-bird", matched: false },
+  { "icon": "fa-solid fa-crow", matched: false },
+  { "icon": "fa-regular fa-crow", matched: false },
+  { "icon": "fa-solid fa-kiwi-bird", matched: false },
+  { "icon": "fa-regular fa-kiwi-bird", matched: false },
+  { "icon": "fa-solid fa-duck", matched: false },
+  { "icon": "fa-regular fa-duck", matched: false }
+  // { "src": "https://img.icons8.com/cotton/128/000000/cat--v4.jpg", matched: false },
+  // { "src": "https://img.icons8.com/cotton/128/000000/dog--v2.jpg", matched: false },
+  // { "src": "https://img.icons8.com/cotton/128/000000/pigeon.jpg", matched: false },
+  // { "src": "https://img.icons8.com/cotton/128/000000/pigeon--v1.jpg", matched: false },
+  // { "src": "https://img.icons8.com/cotton/128/000000/pigeon--v2.jpg", matched: false },
+  // { "src": "https://img.icons8.com/cotton/128/000000/pigeon--v3.jpg", matched: false },
+  // { "src": "https://img.icons8.com/cotton/128/000000/owl--v2.jpg", matched: false },
+  // { "src": "https://img.icons8.com/cotton/128/000000/cow-breed.jpg", matched: false }
 ];
 
 function App() {
