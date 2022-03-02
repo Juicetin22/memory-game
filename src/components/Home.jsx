d700612d260539cf17a5e7abf8f2838200f15792
Justin Huang <justin.2260@gmail.com>
2022-03-02 11:56:25 -0500

    update placement of text and buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Home.scss';
 
 const Home = () => {
 
   return (
     <div className='home'>
+      <div>
       <h2>Welcome!</h2>
       <p>This was a mini self-project that I wanted to make ☺️</p>
       <p>Please choose one of the three memory games to play!</p>
@@ -23,9 +25,8 @@ const Home = () => {
           <Link to="/memoradd">Memoradd</Link>
         </Button>
       </div>
-      <p></p>
-      <p></p>
-      <footer>Icons used in this app can be found at <a href="https://icons8.com/">here</a></footer>
+      </div>
+      <footer className='icons-link'>Icons used in this app can be found at <a href="https://icons8.com/">here</a></footer>
     </div>
   )
 }
