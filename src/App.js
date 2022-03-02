3c5dc542cb600aadcf56457f0260d41c29f18962
Justin Huang <justin.2260@gmail.com>
2022-03-02 01:04:34 -0500

    add routes for app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
+import Home from './components/Home';
 import SimpleMemoryIndex from './components/simple-memory';
+import { Routes, Route, Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <SimpleMemoryIndex />
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/matching-cards'} element={<SimpleMemoryIndex />} />
+      </Routes>
     </div>
   );
 }
