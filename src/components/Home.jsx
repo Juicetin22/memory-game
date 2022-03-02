e84206e3c1c4c128dc571f05f75691836366f276
Justin Huang <justin.2260@gmail.com>
2022-03-02 18:49:44 -0500

    update positioning of text and containers

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,23 +8,23 @@ const Home = () => {
   return (
     <div className='home'>
       <div>
-      <h2>Welcome!</h2>
-      <p>This was a mini self-project that I wanted to make ☺️</p>
-      <p>Please choose one of the three memory games to play!</p>
-      
-      <div className='game-options'>
-        <Button variant='outline-info'>
-          <Link to="/matching-cards">Matching Cards</Link>
-        </Button>
-        <p></p>
-        <Button variant='outline-info'>
-          <Link to="/numbers">Numbers</Link>
-        </Button>
-        {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'} 
-        <Button variant='outline-info'>
-          <Link to="/memoradd">Memoradd</Link>
-        </Button>
-      </div>
+        <h2>Welcome!</h2>
+        <p>This was a mini-project that I wanted to make ☺️</p>
+        <p>Please choose one of the three memory games to play!</p>
+        
+        <div className='game-options'>
+          <Button variant='outline-info'>
+            <Link to="/matching-cards">Matching Cards</Link>
+          </Button>
+          <p></p>
+          <Button variant='outline-info'>
+            <Link to="/numbers">Numbers</Link>
+          </Button>
+          {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'} 
+          <Button variant='outline-info'>
+            <Link to="/memoradd">Memoradd</Link>
+          </Button>
+        </div>
       </div>
       <footer className='icons-link'>Icons used in this app can be found at <a href="https://icons8.com/">here</a></footer>
     </div>
