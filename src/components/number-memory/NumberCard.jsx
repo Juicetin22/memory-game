4441a6d2af3e49e46b72f7010bde0d1ff2eaaee7
Justin Huang <justin.2260@gmail.com>
2022-03-03 00:57:29 -0500

    implement react-card-flip

--- src/components/number-memory/NumberCard.jsx
+++ src/components/number-memory/NumberCard.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NumberCard.scss';
+import ReactCardFlip from 'react-card-flip';
 
 const NumberCard = (props) => {
-  const { card } = props;
+  const { card, setValue } = props;
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+    setValue(card.value);
+  }
+
 
   return (
     <div className='number'>
-      <div>
-        <img src={card.src} alt="number-front" />
+      <ReactCardFlip isFlipped={isFlipped}>
         <img 
           src="https://img.icons8.com/glyph-neue/512/000000/christmas-penguin.png" 
           alt='number-back' 
+          onClick={handleFlip}
         />
-      </div>
+        <img 
+          src={card.src} 
+          alt="number-front" 
+        />
+      </ReactCardFlip>
     </div>
   )
 }
