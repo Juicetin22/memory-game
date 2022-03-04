e1a9b4535aece9fa3b80ea2434db79bd40edde19
Justin Huang <justin.2260@gmail.com>
2022-03-03 22:18:56 -0500

    implement card flipping functionality

--- src/components/memoradd/MemoraddCard.jsx
+++ src/components/memoradd/MemoraddCard.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MemoraddCard.scss';
+import ReactCardFlip from "react-card-flip";
 
 const NumberCard = (props) => {
   const { card } = props;
 
+  const [flipped, setFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setFlipped(!flipped);
+  }
+
   return (
-    <div className='number'>
-      <div>
-        <img src={card.src} alt="number-front" />
+    <div className="memoradd">
+      <ReactCardFlip isFlipped={flipped}>
         <img 
-          src="https://img.icons8.com/glyph-neue/512/000000/christmas-penguin.png" 
-          alt='number-back' 
+          src={card.src} 
+          alt="memoradd-front" 
+          onClick={handleFlip}
         />
-      </div>
+        <img 
+          src="https://img.icons8.com/ios/500/000000/duck.png" 
+          alt='memoradd-back' 
+          onClick={handleFlip}
+        />
+      </ReactCardFlip>
     </div>
   )
 }
