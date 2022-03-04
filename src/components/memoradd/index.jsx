502f3a3b51d88594b4484cc4ddd3fa037a38eb78
Justin Huang <justin.2260@gmail.com>
2022-03-03 22:19:51 -0500

    update text and card placements; update card display and numbers on card

--- src/components/memoradd/index.jsx
+++ src/components/memoradd/index.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import './index.scss';
 import MemoraddCard from "./MemoraddCard";
 import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
 
 const cardNumbers = [
   { "src": "https://img.icons8.com/small/512/000000/1.png", "value": 1 },
@@ -11,26 +12,28 @@ const cardNumbers = [
   { "src": "https://img.icons8.com/small/512/000000/5.png", "value": 5 },
   { "src": "https://img.icons8.com/small/512/000000/6.png", "value": 6 },
   { "src": "https://img.icons8.com/small/512/000000/7.png", "value": 7 },
-  { "src": "https://img.icons8.com/small/512/000000/8.png", "value": 8 }
+  { "src": "https://img.icons8.com/small/512/000000/8.png", "value": 8 },
+  { "src": "https://img.icons8.com/small/512/000000/9.png", "value": 9 }
 ];
 
-const cardNine = { "src": "https://img.icons8.com/small/512/000000/9.png", "value": 9 }
+const cardZero = { "src": "https://img.icons8.com/small/512/26e07f/0.png", "value": 10 };
 
 const MemoraddIndex = () => {
   const [cards, setCards] = useState([]);
-  const [turns, setTurns] = useState(0);
+  const [turn, setTurn] = useState(0);
   const [score, setScore] = useState(0);
+  const [help, setHelp] = useState(3);
 
   // shuffle cards and add an id to each card every new game
   const shuffleCards = () => {
-    const shuffledCards = [...cardNumbers, ...cardNumbers, ...cardNumbers]
+    const shuffledCards = [...cardNumbers, ...cardNumbers, ...cardNumbers, cardZero]
       .map(card => ({ card, sort: Math.random() }))
       .sort((a, b) => a.sort - b.sort)
       .map(({ card }) => card)
       .map((card) => ({ ...card, id: Math.random() }));
 
     setCards(shuffledCards);
-    setTurns(0);
+    setTurn(0);
   };
 
   const displayNumbers = cards.map(card => {
@@ -50,12 +53,23 @@ const MemoraddIndex = () => {
     <>
       <div className="top">
         <Link to="/" className="link"><button className="back-button">← Back</button></Link>
-        <h4>Number Memory Game</h4>
+        <h4>Memoradd</h4>
         <button onClick={shuffleCards} className="new-game">New Game</button>
       </div>
-      <div className="memoradd-grid">
-        {displayNumbers}
-        <MemoraddCard key={9} card={cardNine} />
+      <div className="memoradd-body">
+        <div className="memoradd-grid">
+          {displayNumbers}
+        </div>
+        <div className="memoradd-right-side">
+          <Card className="memoradd-card">
+            <Card.Body>
+              <p>Current Score: {score} </p>
+              <p>Turn: {turn}</p>
+            </Card.Body>
+          </Card>
+          <button>Show Cards</button>
+          <p>Left: {help}</p>
+        </div>
       </div>
     </>
   )
